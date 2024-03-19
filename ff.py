99f2e778421fd06d37d321dce63fe8aaa92f6a91
SalveRobert <160444542+SalveRobert@users.noreply.github.com>
2024-03-19 15:51:19 +0800

    Update ff.py

--- ff.py
+++ ff.py
@@ -1,11 +1,14 @@
-import os
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/", methods = ["GET"])
-def hello():
-    return "goodmorning china now I have binchihlin."
+def home():
+    with open("/app/logs/log.txt","w") as f:
+        f.write("Daemon start!")
+
+    return "OK"
 
 if __name__ == "__main__":
     app.debug = False
-    app.run(host = "0.0.0.0", port = os.getenv("SERVICE_PORT"))
+    app.run(host = "0.0.0.0", port = 5001)
