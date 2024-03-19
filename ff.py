b31b19ef0381203dc8c45e4ac8ad7ab6118ebab0
SalveRobert <160444542+SalveRobert@users.noreply.github.com>
2024-03-19 14:54:47 +0800

    Update ff.py

--- ff.py
+++ ff.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods = ["GET"])
 def hello():
-    return "hello mdfk"
+    return "goodmorning china now I have binchihlin."
 
 if __name__ == "__main__":
     app.debug = False
